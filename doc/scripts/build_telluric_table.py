c77853b3edbecf52c67ac97e082908b3aeaa67b3
Kyle Westfall <westfall@ucolick.org>
2025-10-07 16:19:28 -0700

    check entry parsing

--- doc/scripts/build_telluric_table.py
+++ doc/scripts/build_telluric_table.py
@@ -34,6 +34,10 @@ def tellgrid_table(root, ofile):
 
     # Construct and write the table
     data = [l.split() for l in result.stdout.split('\n')[:-1]]
+    if not all(len(d) == 5 for d in data):
+        raise ValueError(
+            'Unexpected output from `aws` call.  Splitting each entry should yield 5 components.'
+        )
     data = [['File', 'Size', 'Last Modified (UTC)']] + [
         [d[4], ' '.join(d[2:4]), ' '.join(d[:2])] for d in data
     ]
